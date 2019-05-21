44d351193d91836c62f4686d30ff720c0ea0b4b3
SergiAuletMur <36452290+SergiAuletMur@users.noreply.github.com>
2019-05-21 17:06:18 +0200

    Llegeix N fitxers i crea bagOfWords
    LLegeix tots els fitxers paraula a paraula i guarda les paraules, falta filtrar paraules repetides i no guarda subject o body

--- FiltreSpam.java
+++ FiltreSpam.java
@@ -4,7 +4,8 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.File;
-
+import java.util.Scanner;
+import java.util.*;
 
 /**
  *
@@ -12,32 +13,31 @@ import java.io.File;
  */
 
 public class FiltreSpam {
+    public static Vector<String> bagOfWords = new Vector<String>();
 
     public static void main(String[] args) throws IOException {
-        String target_dir = "./SPAM_TRAINING";
+        String target_dir = "./dirProva";
         File dir = new File(target_dir);
         File[] files = dir.listFiles();
 
         for (File f : files) {
             if(f.isFile()) {
                 BufferedReader inputStream = null;
-
-                try {
-                    inputStream = new BufferedReader(
-                                    new FileReader(f));
-                    String line;
-
-                    while ((line = inputStream.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                }
-                finally {
-                    if (inputStream != null) {
-                        inputStream.close();
-                    }
+                
+                Scanner input=new Scanner(f);
+                input.useDelimiter(" +"); //delimitor is one or more spaces
+                while(input.hasNext()){
+                    bagOfWords.addElement(input.next());
+                    //System.out.println(input.next());
                 }
             }
         }
+    
+    // Test bagOfWords que guardi be.
+    Enumeration en = bagOfWords.elements();
+    System.out.println("\nElements are:");
+    while(en.hasMoreElements())
+         System.out.print(en.nextElement() + " ");
     }
 
 }
