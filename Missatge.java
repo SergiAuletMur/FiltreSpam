ac44c4f325fbbe4456c448202c6645acab5fa3d9
Villy16 <36771855+Villy16@users.noreply.github.com>
2019-06-01 18:15:45 +0200

    peneee

---
+++ Missatge.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filtrespam;
+
+
+/**
+ *
+ * @author Oulet
+ */
+public class Missatge {
+    public String paraula;
+    public Integer nCops;
+    
+    public Missatge(String par){
+        paraula = par;
+        nCops = 1;
+    }
+}
